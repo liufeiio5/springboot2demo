2cae1cdc7051a55ab5684fd3259d852fc28490de
fq <18088000805@163.com>
2018-11-21 15:18:28 +0800

    合并dev

--- src/main/java/com/zftx/mcdaily/controller/TestController.java
+++ src/main/java/com/zftx/mcdaily/controller/TestController.java
@@ -5,14 +5,10 @@ import com.zftx.mcdaily.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import java.util.List;
 
 @Controller
 public class TestController {
-
     @Autowired
     UserService userService;
 
