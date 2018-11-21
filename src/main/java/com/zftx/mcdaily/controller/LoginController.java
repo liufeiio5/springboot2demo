e44db15a3267b749028a21542b40e59c3d4401ba
buzhifeng <bzhf7330@micangpay.com>
2018-11-21 14:05:12 +0800

    添加登录逻辑

--- src/main/java/com/zftx/mcdaily/controller/LoginController.java
+++ src/main/java/com/zftx/mcdaily/controller/LoginController.java
@@ -1,8 +1,11 @@
 package com.zftx.mcdaily.controller;
 
+import com.zftx.mcdaily.bean.User;
 import com.zftx.mcdaily.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class LoginController {
@@ -10,5 +13,14 @@ public class LoginController {
     @Autowired
     private UserService userService;
 
+    /**
+     * 用户登录
+     * @param user
+     * @return
+     */
+    @RequestMapping(value = "/login",method = RequestMethod.GET)
+    public String login(User user){
+        return "login";
+    }
 
 }
