e1333032301f93fea0449b37a45f59154bf4865c
fq <18088000805@163.com>
2018-11-21 19:07:35 +0800

    职位增删改查

--- src/main/java/com/zftx/mcdaily/controller/TestController.java
+++ src/main/java/com/zftx/mcdaily/controller/TestController.java
@@ -1,9 +1,5 @@
 package com.zftx.mcdaily.controller;
 
-import com.zftx.mcdaily.bean.Event;
-import com.zftx.mcdaily.service.EventService;
-import com.zftx.mcdaily.util.R;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
