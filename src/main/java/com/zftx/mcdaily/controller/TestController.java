b426a44f41eb475525bffe0587ccac8f9bd9e7d0
fq <18088000805@163.com>
2018-11-21 15:08:39 +0800

    user增查

--- src/main/java/com/zftx/mcdaily/controller/TestController.java
+++ src/main/java/com/zftx/mcdaily/controller/TestController.java
@@ -1,11 +1,20 @@
 package com.zftx.mcdaily.controller;
 
+import com.zftx.mcdaily.bean.User;
+import com.zftx.mcdaily.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 @Controller
 public class TestController {
+
+    @Autowired
+    UserService userService;
 
     @RequestMapping("/show")
     @ResponseBody
@@ -17,4 +26,5 @@ public class TestController {
     public String toIndex(){
         return "index";
     }
+
 }
