f8d42a5897b9c4ba93491090cf2a3303cc72ecb1
buzhifeng <bzhf7330@micangpay.com>
2018-11-24 17:55:35 +0800

    删除注册多余代码

--- src/main/java/com/zftx/mcdaily/controller/LoginController.java
+++ src/main/java/com/zftx/mcdaily/controller/LoginController.java
@@ -69,7 +69,6 @@ public class LoginController {
     @RequestMapping(value = "/register",method = RequestMethod.POST)
     @ResponseBody
     public R registers(String userName,String password,String fullName,String email,String phone,String birthplace,String birthday,String position,String hobby,String motto){
-//        user.setPassword(MD5.md5(user.getPassword(),user.getUserName()));
         String result = userService.insertUser(new User().setPassword(MD5.md5(password,userName)).setUserName(userName).setFullName(fullName).setEmail(email).setPhone(phone).
         setBirthplace(birthplace).setBirthday(birthday).setPosition(position).setHobby(hobby).setMotto(motto));
         if(result.equals("success")){
