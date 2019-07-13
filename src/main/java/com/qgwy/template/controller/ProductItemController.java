0a73c84c211f06920009ed8d0ce63e31584ec1eb
buzhifeng <zhifengbu@outlook.com>
2019-07-13 09:10:34 +0800

    多表联查

--- src/main/java/com/qgwy/template/controller/ProductItemController.java
+++ src/main/java/com/qgwy/template/controller/ProductItemController.java
@@ -4,12 +4,14 @@ import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.qgwy.template.bean.ProductItem;
 import com.qgwy.template.service.ProductItemService;
+import com.qgwy.template.util.R;
 import com.qgwy.template.vo.ProductDetailVo;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -27,24 +29,24 @@ public class ProductItemController {
 
 
     @ResponseBody
-    @GetMapping("/getProductDetails")
+    @PostMapping("/getProductDetails")
     @ApiOperation(value = "多表联查分页",notes = "查询商品的详细信息")
-    public Map<String,Object> getProductDetails(Integer page,Integer size,Integer marketId,Integer categoryId){
+    public R getProductDetails(Integer page,Integer size,Integer marketId,Integer categoryId){
         Map<String,Object> map = new HashMap<>();
         Page<ProductDetailVo> page1 = new Page<>(page,size);
         Page<ProductDetailVo> detailVoPage = productItemService.getProductDetail(page1,marketId,categoryId);
         map.put("data",detailVoPage);
-        return map;
+        return R.ok().put("data",detailVoPage);
     }
 
     @ResponseBody
-    @GetMapping("/getProductItemList")
+    @PostMapping("/getProductItemList")
     @ApiOperation(value = "单表分页，使用mybatis-plus自带的分页",notes = "单表查询商品的基本信息")
-    public Map<String,Object> getProductItemList(Integer page,Integer size,Integer marketId){
+    public R getProductItemList(Integer page, Integer size, Integer marketId){
         Map<String,Object> map = new HashMap<>();
         IPage<ProductItem> productItemPage = productItemService.getProductItemList(new Page<>(page,size),marketId);
         List<ProductItem> list = productItemPage.getRecords();
         map.put("data",productItemPage);
-        return map;
+        return R.ok().put("data",productItemPage);
     }
 }
