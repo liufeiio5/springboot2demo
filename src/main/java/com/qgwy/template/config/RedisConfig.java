a51b01c88dae0e7435e2acfa66178e228a763a8f
刘自持 <liufeiio5@163.com>
2019-07-15 10:56:26 +0800

    集成redis、rabbitmq

---
+++ src/main/java/com/qgwy/template/config/RedisConfig.java
@@ -0,0 +1,23 @@
+package com.qgwy.template.config;
+
+import com.qgwy.template.bean.User2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+
+import java.net.UnknownHostException;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    public RedisTemplate<Object, User2> myRedisTemplate(RedisConnectionFactory redisConnectionFactory) throws UnknownHostException {
+        RedisTemplate<Object, User2> myRedisTemplate = new RedisTemplate<>();
+        myRedisTemplate.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer<User2> jacksonSer = new Jackson2JsonRedisSerializer<User2>(User2.class);
+        myRedisTemplate.setDefaultSerializer(jacksonSer);
+        return myRedisTemplate;
+    }
+}
