ed22a6b08bbf5c22c36606066a86a17a5733d7ce
buzhifeng <zhifengbu@outlook.com>
2019-07-24 16:18:12 +0800

    版本回退

--- src/main/java/com/qgwy/alpha_web_manager/service/Impl/CbecAreaServiceImpl.java
+++ src/main/java/com/qgwy/alpha_web_manager/service/Impl/CbecAreaServiceImpl.java
@@ -1,10 +1,14 @@
 package com.qgwy.alpha_web_manager.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.qgwy.alpha_web_manager.bean.CbecArea;
 import com.qgwy.alpha_web_manager.mapper.CbecAreaMapper;
 import com.qgwy.alpha_web_manager.service.CbecAreaService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 /**
  * <p>
@@ -17,4 +21,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class CbecAreaServiceImpl extends ServiceImpl<CbecAreaMapper, CbecArea> implements CbecAreaService {
 
+    @Autowired
+    private CbecAreaMapper cbecAreaMapper;
+
+    /**
+     * 测试
+     * @return
+     */
+    @Override
+    public List<CbecArea> getList(){
+        return cbecAreaMapper.selectList(new QueryWrapper<>());
+    }
 }
